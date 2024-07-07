a8996ffbf1132a7c9d7a8b15166a4fa2bb57ca44
frosterezz <frosterezz@gmail.com>
2024-07-07 22:42:46 +0800

    Add files via upload

---
+++ Kursus Ajik/main.js
@@ -0,0 +1,38 @@
+function toggleDetail(e) {
+    const target = $(e.target)
+
+    if($(target).hasClass ("active")) {
+        $(target).html("More Info").removeClass("active")
+    } else{
+        $(target).html("Less Info").addClass("active")
+    }
+
+    const item = $(target).parents(".about-exp-item")
+    const detail = $(item).children(".about-exp-item-detail")
+
+    $(detail).slideToggle()
+}
+
+function onFormSubmit(e){
+    e.preventDefault()
+    const email = $("#inp_email")
+    const telp = $("#inp_telp")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()) {
+        alert("E-mail is Required")
+    } else if(!$(telp).val()) {
+        alert("Phone Number is Required")
+    }  else if(!$(subject).val()) {
+        alert("Subject is Required")
+    } else if(!$(message).val()) {
+        alert("Message is Required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(telp).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
